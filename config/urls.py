1dda251026cff1842fb5ae696e7abebf97967869
angelchiav <achiavicuna@gmail.com>
2025-08-04 20:14:06 -0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
-    path('api/posts/', include('apps.posts.urls'))
+    path('api/posts/', include('apps.posts.urls')),
+    path('api/comments', include('apps.comments.urls'))
 ]
